cd606ec9bc3057accf5d7129d415fc47dba4396e
AlbertoSafanda <albertosafanda89@gmail.com>
2025-11-23 23:52:42 +0700

    Add migration and controller

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validasi input
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        // Membuat user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        // Login setelah registrasi
+        Auth::login($user);
+
+        // Kirim email verifikasi
+        $user->sendEmailVerificationNotification();
+
+        // Arahkan ke halaman untuk cek email
+        return redirect()->route('verification.notice');
+    }
+}
